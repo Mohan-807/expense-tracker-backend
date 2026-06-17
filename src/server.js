ae6f73b80a476cc94987b26b43f52d3bb4e6b23e
Mohan <mohan8105072435@gmail.com>
2026-06-17 07:44:46 +0530

    prisma configuration

--- src/server.js
+++ src/server.js
@@ -1,24 +1,18 @@
 import app from "./app.js";
+import prisma from "./config/prisma.js";
 import { env } from "./config/env.js";
-import logger from "./config/logger.js";
-import { pool } from "./config/db.js";
-
-app.listen(env.PORT, () => {
- logger.info(
-  `Server running on port ${env.PORT}`
-);
-});
 
 async function startServer() {
   try {
-    await pool.query("SELECT NOW()");
-    console.log("Database connected successfully");
+    await prisma.$connect();
+
+    console.log("Database Connected");
 
     app.listen(env.PORT, () => {
-      console.log(`Server running on port ${env.PORT}`);
+      console.log(`Server running on ${env.PORT}`);
     });
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error(error);
   }
 }
 
